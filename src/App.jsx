95dc75e6ac55bc752b1bdf5f6ade62c8b2a6e633
ROmain gabylone <eromixx69@gmail.com>
2026-01-15 13:41:59 +0100

    Création des composants et page ReaCT

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
---
+++ src/components/FeatureItem.jsx
@@ -0,0 +1,11 @@
+function FeatureItem({ icon, title, description }) {
+  return (
+    <div className="feature-item">
+      <img src={icon} alt="Feature Icon" className="feature-icon" />
+      <h3 className="feature-item-title">{title}</h3>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+export default FeatureItem
